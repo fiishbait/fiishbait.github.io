d3201e1a6e2247a6626c90ea8a8765a82956841a
fiishbait <168551490+fiishbait@users.noreply.github.com>
2025-06-13 12:24:07 +1000

    h

--- assignment3/script.js
+++ assignment3/script.js
@@ -7,41 +7,52 @@ const mainCat = document.querySelector(".cat");
 
 let currentFoodId = null;
 
-// Hide button initially
+// Hides button when bowl is empty
 eatButton.style.display = "none";
 
-// Setup drag and drop
+// Drag and drop
+
+// Items are enabled to be dragged around
 draggables.forEach((item) => {
   item.addEventListener("dragstart", (e) => {
     e.dataTransfer.setData("text/plain", item.id);
 
+    // Copies the icon of the dragged item
     const dragIcon = item.cloneNode(true);
     dragIcon.style.width = "100px";
     dragIcon.style.position = "absolute";
-    dragIcon.style.top = "-1000px";
+    dragIcon.style.top = "-1000px"; // Able to move the dragged food item off-screen
     document.body.appendChild(dragIcon);
+
+    // Customises the dragged image
     e.dataTransfer.setDragImage(dragIcon, 50, 50);
     setTimeout(() => {
+      // Gets rid of the copied icon
       document.body.removeChild(dragIcon);
     }, 0);
   });
 });
 
+// Allows the bowl to become the dropbox
 bowl.addEventListener("dragover", (e) => e.preventDefault());
 
+// Dropping the foods in the bowl
 bowl.addEventListener("drop", (e) => {
   e.preventDefault();
+
+  // Gets the ID of the food
   const id = e.dataTransfer.getData("text/plain");
   const draggedItem = document.getElementById(id);
 
   if (draggedItem) {
     currentFoodId = id;
+
+    // Changes the image of the bowl to the food that has been dropped
     bowl.src = draggedItem.dataset.bowl;
 
-    // Only show button if a valid food is dropped
+    // Shows the eat button when food is dropped into the bowl
     if (draggedItem.dataset.bowl && draggedItem.dataset.reaction) {
       eatButton.style.display = "block";
-      positionEatButton();
     }
   }
 });
@@ -53,21 +64,22 @@ function positionEatButton() {
   eatButton.style.left = `${window.scrollX + rect.right + -265}px`;
 }
 
-// Eat button click
+// When eat button is clicked
 eatButton.addEventListener("click", () => {
+  // No food has been dropped, it will do nothing
   if (!currentFoodId) return;
 
+  // Gets reaction of the dropped food
   const draggedItem = document.getElementById(currentFoodId);
   const reaction = draggedItem.dataset.reaction;
 
-  // Optional: swap cat image
+  // Swaps cat image based on the reaction of the cat to the food that has been dropped
   const mainCat = document.querySelector(".cat");
-  if (reaction === "happy") mainCat.src = "happy cat.png";
-  else if (reaction === "angry") mainCat.src = "angry cat.png";
-
-  showReaction(reaction);
-
-  bowl.src = "bowl.png";
-  eatButton.style.display = "none";
-  currentFoodId = null;
+  if (reaction === "happy") {
+    mainCat.src = "happy cat.png";
+    showReaction("happy");
+  } else if (reaction === "angry") {
+    mainCat.src = "angry cat.png";
+    showReaction("angry");
+  }
 });
